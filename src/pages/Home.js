a5eefa421fa4d079226650df76afd135fe2037f8
kscheltema <k87scheltema@outlook.com>
2021-09-11 09:04:21 +0200

    basic AppBar in place

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,28 @@
 import {Fragment} from "react";
-import {Grid, Hidden} from "@material-ui/core";
+import {AppBar, Button, Grid, Hidden, IconButton, Toolbar, Typography} from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
 import styles from "../ui/theme";
 
 function Home() {
     const classes = styles();
+    
     return (
         <Fragment>
             <Hidden smDown>
-            <section className="Home Small">
+            <section className="root">
                 <Grid container md={12} justify="center" direction="row">
-                
+<AppBar position="static">
+  <Toolbar>
+    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      <MenuIcon />
+    </IconButton>
+    <Typography variant="h6" className={classes.title}>
+      Seth's Poetry
+    </Typography>
+    <Button color="inherit">Login</Button>
+    <Button color="inherit">Sing Up</Button>
+  </Toolbar>
+</AppBar>
                 </Grid>
             </section>
             </Hidden>
--- src/ui/theme.js
+++ src/ui/theme.js
@@ -1,9 +1,86 @@
-import { makeStyles } from "@material-ui/core";
-import { createStyles } from "@material-ui/styles";
-
-const styles = makeStyles((theme) => 
+import {
+    createTheme,
+    createStyles,
+    makeStyles,
+  } from "@material-ui/core/styles";
+  
+  export const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#019cde",
+      },
+      secondary: {
+        main: "#fff",
+      },
+    },
+    typography: {
+      fontFamily: "Roboto",
+    },
+  });
+  
+  const styles = makeStyles((theme) =>
     createStyles({
-
+    //   bgContainer: {
+    //     backgroundPosition: 'center',
+    //     backgroundSize: 'cover',
+    //     display: 'block',
+    //     position: 'fixed',
+    //     objectFit: 'cover',
+    //     left: '0px',
+    //     width: '100%',
+    //     zIndex: '-1',
+    //     [theme.breakpoints.up('sm')]: {
+    //       backgroundImage: `url('${BgLandscape}')`,
+    //       height: '100%',
+    //     },
+    //     [theme.breakpoints.down('sm')]: {
+    //       height: '100%',
+    //       backgroundImage: `url('${BgPortrait}')`,
+    //     },
+    //   },
+    //   buttonStyle: {
+    //     borderRadius: 10,
+    //     color: theme.palette.secondary["main"],
+    //     backgroundColor: theme.palette.primary["main"],
+    //     padding: "0 0rem",
+    //     width: "50em",
+    //     margin: "0.5rem 0rem 0.5rem 0rem",
+    //     textTransform: "none",
+    //   },
+    //   homeLogo: {
+    //     paddingTop: "1em",
+    //     width: "20em",
+    //   },
+    //   homeText: {
+    //     color: theme.palette.secondary["main"],
+    //   },
+    //   homeMessageMobile: {
+    //     fontSize: 20,
+    //     fontWeight: "bold",
+    //     paddingLeft: "5em",
+    //     paddingRight: "5em",
+    //     paddingBottom: "5em",
+    //     paddingTop: "5em",
+    //   },
+    //   homeMessageSpacing: {
+    //     alignItems: "center",
+    //   },
+    //   copyright: {
+    //     textAlign: "center",
+    //     fontSize: 14,
+    //     fontWeight: "bold",
+    //     paddingBottom: "3rem",
+    //   },
+    root: {
+        flexGrow: 1,
+      },
+    menuButton: {
+        marginRight: theme.spacing(2),
+      },
+    title: {
+        flexGrow: 1,
+      },
     })
-    );
-    export default styles;
+  );
+  
+  export default styles;
